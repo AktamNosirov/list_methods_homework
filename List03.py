feda2fb8dbbe5c88dfc3f5192601041fe100b135
AktamNosirov <94_aktam@mail.ru>
2022-04-02 12:49:45 +0500

    1-3

--- List03.py
+++ List03.py
@@ -7,7 +7,7 @@ def main(fruits1, fruits2):
     Returns:
         list: return answer
     """
-    return fruits2+fruits1
+    return fruits1+fruits2
 fruits1 = ["olma", "uzum", "anor"] 
 fruits2 = [ "behi", "shaftoli"] 
 print(main(fruits1,fruits2))
